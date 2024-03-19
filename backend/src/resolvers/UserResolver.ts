e596a51f1116d608caff7da507f103ec1d4a7864
HpJkR <m.hertereau@gmail.com>
2024-03-19 11:02:43 +0100

    Co-authored-by: Alicia Cqt <aliciacqt@users.noreply.github.com> Co-authored-by: Loïc Hernandez <xakaz@users.noreply.github.com>

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,5 +1,9 @@
+import { verify } from "argon2";
+import { GraphQLError } from "graphql";
+import jwt from "jsonwebtoken";
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import User, { NewUserInput } from "../entities/User";
+import User, { LoginInput, NewUserInput } from "../entities/User";
+import env from "../env";
 
 @Resolver()
 class UserResolver {
@@ -7,6 +11,11 @@ class UserResolver {
   async createUser(
     @Arg("newUserData", { validate: true }) newUserData: NewUserInput
   ) {
+    const existingUser = await User.findOneBy({ email: newUserData.email });
+    if (existingUser) {
+      throw new GraphQLError("User with this email already exists");
+    }
+
     const newUser = new User();
     Object.assign(newUser, newUserData);
     const newUserWithId = await newUser.save();
@@ -16,6 +25,26 @@ class UserResolver {
   @Query(() => [User])
   async getUsers() {
     return User.find();
+  }
+
+  @Mutation(() => String)
+  async login(@Arg("data") data: LoginInput) {
+    const existingUser = await User.findOneBy({ email: data.email });
+    if (existingUser === null) {
+      throw new GraphQLError("Invalid credentials");
+    }
+    const isPasswordValid = await verify(
+      data.password,
+      existingUser.hashedPassword
+    );
+    if (!isPasswordValid) {
+      throw new GraphQLError("Invalid credentials");
+    }
+
+    const token = jwt.sign({ userId: existingUser.id }, env.JWT_PRIVATE_KEY, {
+      expiresIn: "1h",
+    });
+    return token;
   }
 }
 
